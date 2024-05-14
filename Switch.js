a33f756251c0f4febbc1cc38f447faf4c64ea131
Muhammad Abdussalam <masalami042@gmail.com>
2024-05-14 03:15:50 +0100

    Practicing switch statement

---
+++ Switch.js
@@ -0,0 +1,60 @@
+//Switch is another form of if else statement: EG.
+let familyName = 'usman';
+/*if (familyName == 'Usman') {
+    console.log('That is true');
+} else {
+    console.log('Unfortunately, That is wrong');
+}
+*/
+
+switch (familyName) {
+    case 'usman':
+        console.log('That is true');
+        break;
+    case 'Katibi':
+        console.log('That is my surname');
+        break;
+    default:
+        console.log('That is false');
+        break;
+}
+
+let passMark = 6;
+
+switch (true){
+    case passMark > 40:
+        console.log('You passed');
+        break;
+    default:
+        console.log('You failed');
+        break;
+}
+
+// switch statement determines your evening routine based on the day of the week
+let day = 'Sunday'
+switch(day){
+    case 'Monday':
+    console.log('Free day');
+    break;
+    case 'Tuesday':
+    console.log('Went to the Market');
+    break;
+    case 'Wednesday':
+    console.log('coding!');
+    break;
+    case'Thursday':
+    console.log('Go to school');
+    break;
+    case'Friday':
+    console.log('Go to mosque');
+    break;
+    case'Saturday':
+    console.log('Stay at home');
+    break;
+    case'Sunday':
+    console.log('Go to madrasah');
+    break;
+    default:
+    console.log('There is no such day')
+    break;
+}
