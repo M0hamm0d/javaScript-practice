6192da5aaa659ce09507fc8255489f4deb92e40d
Muhammad Abdussalam <masalami042@gmail.com>
2024-05-22 21:47:41 +0100

    Practicing DOM

---
+++ DOM.js
@@ -0,0 +1,8 @@
+/*alert(location.href)
+if (confirm('Go to Wikipidea')){
+    location.href = "https://wikipedia.org";
+}
+*/
+// for (let i = 0; i < document.body.childNodes.length; i++) {
+//     alert( document.body.childNodes[i] ); // Text, DIV, Text, UL, ..., SCRIPT
+// }
